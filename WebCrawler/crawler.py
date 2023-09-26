c5733cbb87d85c0541488621c938534b2ccf26d6
Carl-97 <cape1903@student.miun.se>
2023-09-26 15:21:30 +0200

    fixes

--- WebCrawler/crawler.py
+++ WebCrawler/crawler.py
@@ -32,19 +32,41 @@ class WebCrawler:
         self.driver.get(url)
         return self.driver.page_source
 
-    # tar bort delar av hemsidan
-    def get_html_body(self, url):
-        # finds and remove header and
-        header_content = self.driver.find_element_by_tag_name("header").text
-        footer_content = self.driver.find_element_by_tag_name("footer").text
-        navbar_content = self.driver.find_element_by_tag_name("navbar").text
-        self.driver.execute_script("arguments[0].parentNode.removeChild(arguments[0])", header_content)
-        self.driver.execute_script("arguments[0].parentNode.removeChild(arguments[0])", footer_content)
-        self.driver.execute_script("arguments[0].parentNode.removeChild(arguments[0])", navbar_content)
+    def has_product(self, html_content):
+        soup = BeautifulSoup(html_content, 'html.parser')
+        # product = soup.find('product')
+        selector = 'div[class*="product"]'
+        matching_div = soup.select(selector)
+        if matching_div:
+            print("Finns")
+            return True
+        selector2 = 'div[id*="product"]'
+        matching_div = soup.select(selector2)
+        if matching_div:
+            print("Finns")
+            return True
+        return False
 
-        # get body content
-        body_content = self.driver.find_element_by_tag_name("body").text
-        return body_content
+    def get_html_body(self, html_content):
+        soup = BeautifulSoup(html_content, 'html.parser')
+        # gets all 'a' elements that have a href atribute
+
+        header = soup.find('header')
+        if header:
+            header.extract()
+        footer = soup.find('footer')
+        if footer:
+            footer.extract()
+        # nav = soup.find('nav')
+        # if nav:
+        # nav.extract()
+
+        navbar = soup.find(class_='navbar')
+        if navbar:
+            navbar.extract()
+
+        body = soup.find('body').get_text()
+        return body
 
     # Find valid links only, ex: links that starts with 'www'
     def find_links(self, html_content):
@@ -123,12 +145,14 @@ class WebCrawler:
                 print("curently on: ", current_url)
                 self.visited.append(current_url)
                 html_content = self.get_html_content(current_url)
-                # TODO: optimize extraction, get_html_content and extract_text_content
-                body = self.get_html_content(current_url)
+                # TODO: optimize extraction, get_html_body and extract_text_content
                 self.add_links(html_content, current_depth)
-                self.save_to_csv(current_url,'visited.csv')
+                self.save_to_csv(current_url, 'resources/visited.csv')
                 text_content = "this url:" + self.url
-                text_content += self.extract_text_content(body)
+                if self.has_product(html_content):
+                    text_content += self.get_html_body(html_content)
+                #text_content = "this url:" + self.url
+                #text_content += self.extract_text_content(body)
                 self.save_to_csv(text_content, csv_filename)
                 # saves it to the visited csv file
                 current_depth += 1
@@ -138,17 +162,27 @@ class WebCrawler:
         url2 = ('https://www.rskdatabasen.se/sok?Query=' + str(id_nr))
         html_content = self.get_html_content(url)
         text_content = "this url:" + self.url
-        text_content += self.extract_text_content(html_content)
+        #text_content += self.extract_text_content(html_content)
+        if self.has_product(html_content):
+            text_content += self.get_html_body(html_content)
         self.save_to_csv(text_content, file)
 
     def add_links(self, html_content, current_depth):
         # TODO: prints the links found on the page
         links = self.find_links(html_content)
-            
-        #print(f"Found Links at depth {current_depth}:")
         for link in links:
-            #print(link)
             self.link_queue.put(link)
 
     def close_browser(self):
         self.driver.quit()
+
+
+# Testing for some methods
+if __name__ == '__main__':
+    file = 'test_method.csv'
+    depth = 1
+    url = 'https://www.e-nummersok.se/lista/plintsystem-plint-och-kabelmarkning-apparats/plintsystem-for-skenmontage/weidmuller/plint-sakr-pa-m-2920154-14020'
+
+    wc = WebCrawler()
+    wc.set_url(url)
+    wc.crawl_website_with_depth(file, depth)
