b825284a434d427116db8ecc6f13541c0ccdceb1
jakob <me@example.com>
2023-09-26 10:17:17 +0200

    test

--- WebCrawler1.2/enummerSite.py
+++ WebCrawler1.2/enummerSite.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from crawler import WebCrawler
 
-excel_file = 'e-nummer_20230915.xlsx'
+excel_file = 'e-nummer.xlsx'
 
 df = pd.read_excel(excel_file)
 crawlerSize= 0; #knowing a  mount of items can be useful #21217
