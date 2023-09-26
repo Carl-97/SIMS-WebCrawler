e13c9def7d63819d14e39f1ac8bb7d45dca1b305
Carl-97 <cape1903@student.miun.se>
2023-09-26 11:37:12 +0200

    Added Excelmanip folder, not merged with webcrawler yet though added a method in crawler that just scrapes e-number sites

--- WebCrawler1.2/enummerSite.py
+++ WebCrawler1.2/enummerSite.py
@@ -3,35 +3,38 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from crawler import WebCrawler
 
-excel_file = 'e-nummer_20230915.xlsx'
 
-df = pd.read_excel(excel_file)
-crawlerSize= 0; #knowing a  mount of items can be useful #21217
-i = 0 
-crawlertest = []
-for index, row in df.iterrows():
-    
-    #asuming we have thise rows in the excel file
-    #startAritikel = row['Artikelnummer']
-    #startArtikelbeskrivning = row['Artikelbeskrivning']
-    #startTypbeteckning = row['Typbeteckning']
-    #startKompletterandeInfo = row['Kompletterande info']
-    #startVarumärke= row['Varumärke']
-    Size = len(crawlertest) 
-    starEnumer = row['E-nummer']
-    crawlertest.append( WebCrawler()) 
-    startUrl = ('https://www.e-nummersok.se/sok?Query='+ str(starEnumer))
-    #https://www.e-nummersok.se/sok?Query=2920154
-    filename = 'web_data.csv'
-    #deep is amount of websites url we searched from 
-    depth = 10
-    crawlertest[i].setUrl(startUrl)
-    crawlertest[i].crawl_website_with_depth(filename, depth)
-    array = crawlertest[i].get_visited()
-    for x in array:
-        print(x)
-    i = i + 1
+if __name__ == "__main__":
+    excel_file = 'resources/sample.xlsx'
 
-i=0
-for crawler in crawlertest[i]:
-    crawler[i].close_browser()
+    df = pd.read_excel(excel_file)
+    crawlerSize = 0  # knowing amount of items can be useful #21217
+    i = 0
+    crawlertest = []
+    for index, row in df.iterrows():
+
+
+        #''asuming we have these rows in the excel file
+        #startAritikel = row['Artikelnummer']
+        #startArtikelbeskrivning = row['Artikelbeskrivning']
+        #startTypbeteckning = row['Typbeteckning']
+        #startKompletterandeInfo = row['Kompletterande info']
+        #startVarumärke= row['Varumärke']
+        Size = len(crawlertest)
+        starEnumer = row['E-nummer']
+        crawlertest.append(WebCrawler())
+        startUrl = ('https://www.e-nummersok.se/sok?Query='+ str(starEnumer))
+        #https://www.e-nummersok.se/sok?Query=2920154
+        filename = 'web_data.csv'
+        #deep is amount of websites url we searched from
+        depth = 10
+        crawlertest[i].setUrl(startUrl)
+        crawlertest[i].crawl_website_with_depth(filename, depth)
+        array = crawlertest[i].get_visited()
+        for x in array:
+            print(x)
+        i = i + 1
+
+    i = 0
+    for crawler in crawlertest[i]:
+        crawler[i].close_browser()
