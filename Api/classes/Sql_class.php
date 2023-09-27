5efb9a389d70c4e547415fbdcd1594dcb93a691f
jakob <me@example.com>
2023-09-27 15:32:54 +0200

    Trying to make a api

---
+++ Api/classes/Sql_class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * class Sql
+ * responsibility for sql quarys
+ * @author jakob Hallin
+ */
+class Sql{
+    /**
+     * prepares and execute sql quaries
+     * @return $stmt 
+     */
+	public function execute($sql){
+			//$con = $this->con;
+			$db = new Db;
+			$con = $db->connect();
+			$stmt = $con->prepare($sql);
+			$stmt ->execute();
+			return $stmt;
+	}	
+		
+	/**
+     * prepares and execute sql quaries to statment to insert
+     */
+	public function insert($sql, $arg1, $arg2){
+		$db = new Db;
+		$con = $db->connect();
+        $stmt = $con->prepare($sql);
+        $stmt->execute([$arg1, $arg2]);
+	}
+}
