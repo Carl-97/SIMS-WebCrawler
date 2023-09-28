83898d103fb744c8b059497b5c99b7107579ec19
jakob <me@example.com>
2023-09-28 17:20:28 +0200

    update crawler to 1.3

--- WebCrawler/enummerSite.py
+++ WebCrawler/enummerSite.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from crawler import WebCrawler
 
-excel_file = '/resources/e-nummer_20230915.xlsx'
+excel_file = 'e-nummer.xlsx'
 
 df = pd.read_excel(excel_file)
 crawlerSize= 0; #knowing a  mount of items can be useful #21217
@@ -25,7 +25,7 @@ for index, row in df.iterrows():
     filename = 'web_data.csv'
     #deep is amount of websites url we searched from 
     depth = 10
-    crawlertest[i].setUrl(startUrl)
+    crawlertest[i].set_url(startUrl)
     crawlertest[i].crawl_website_with_depth(filename, depth)
     array = crawlertest[i].get_visited()
     for x in array:
