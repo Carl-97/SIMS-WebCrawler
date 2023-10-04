5b0763af61614960041b133d090fe5a4ab058d05
jakob <me@example.com>
2023-10-04 13:38:40 +0200

    updated to carls newest scraper

--- WebCrawler/enummerSite.py
+++ WebCrawler/enummerSite.py
@@ -1,68 +1,37 @@
-from crawler import WebCrawler
 import pandas as pd
-import csv
-  
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from crawler import WebCrawler
 
-inputfile = './resources/apendo.xlsx'
-outputfile = 'web_data.csv'
-rskoutputfile = 'rks.csv'
-enumoutputfile = 'enum.csv'
-    #url = 'https://www.e-nummersok.se/lista/plintsystem-plint-och-kabelmarkning-apparats/plintsystem-for-skenmontage/weidmuller/plint-sakr-pa-m-2920154-14020'
+excel_file = 'e-nummer.xlsx'
 
-    #excel_file = '/resources/e-nummer_20230915.xlsx'
-
-
-df = pd.read_excel(inputfile)
+df = pd.read_excel(excel_file)
 crawlerSize= 0; #knowing a  mount of items can be useful #21217
 i = 0 
 crawlertest = []
-ignore = []
-#apendar alla från ignore filen
-#ignore.append("google.com")
-with open('ignore.csv') as file_obj:
-      
-    # Create reader object by passing the file 
-    # object to reader method
-    reader_obj = csv.reader(file_obj)
-      
-    # Iterate over each row in the csv 
-    # file using reader object
-    for row in reader_obj:
-        print(row)
-        ignore.append(row)
-
-
-
 for index, row in df.iterrows():
-    wc = WebCrawler(ignore)
-  
     
- 
+    #asuming we have thise rows in the excel file
+    #startAritikel = row['Artikelnummer']
+    #startArtikelbeskrivning = row['Artikelbeskrivning']
+    #startTypbeteckning = row['Typbeteckning']
+    #startKompletterandeInfo = row['Kompletterande info']
+    #startVarumärke= row['Varumärke']
     Size = len(crawlertest) 
-    starEnumer = row['Artikelnummer']
-    
-    crawler = WebCrawler(ignore)
-    
-    #crawlertest.append( WebCrawler()) 
-    
+    starEnumer = row['E-nummer']
+    crawlertest.append( WebCrawler()) 
     startUrl = ('https://www.e-nummersok.se/sok?Query='+ str(starEnumer))
-    
-    #
-    # deep is amount of websites url we searched from 
-    
-    depth = 0
-    crawlertest.append(crawler)
-    #check if its enum or rskhttps://www.e-nummersok.se/sok?Query=100019939
-    #its not
-    #sök artikel + brand
+    #https://www.e-nummersok.se/sok?Query=2920154
+    filename = 'web_data.csv'
+    #deep is amount of websites url we searched from 
+    depth = 10
     crawlertest[i].set_url(startUrl)
-    crawlertest[i].crawl_website_with_depth(outputfile, depth, startUrl)
-    
+    crawlertest[i].crawl_website_with_depth(filename, depth)
     array = crawlertest[i].get_visited()
     for x in array:
         print(x)
     i = i + 1
 
-y=0
-for crawler in crawlertest[y]:
-    crawler[y].close_browser()
+i=0
+for crawler in crawlertest[i]:
+    crawler[i].close_browser()
