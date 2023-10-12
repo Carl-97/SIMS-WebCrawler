3d55fe6b0653e59edb1753549316054d42bfb71e
Carl-97 <cape1903@student.miun.se>
2023-10-12 16:03:06 +0200

    changed get_html_content() to retry if fail fetching

--- test2.py
+++ test2.py
@@ -1,9 +1,27 @@
+from urllib.parse import quote
 from ExcelManip import excelmanip as em
 from WebCrawler.crawlerDemo4 import WebCrawler
-from urllib.parse import quote
-import os
-import openpyxl
-from openpyxl.styles import PatternFill
+
+
+def process_with_id(id_val, web_crawler, index):
+    websites = [
+        f'https://www.e-nummersok.se/sok?Query={id_val}',
+        f'https://www.rskdatabasen.se/sok?Query={id_val}'
+    ]
+    for url in websites:
+        web_crawler.crawl_website_with_depth(str(index), 0, start_url=url)
+
+
+def process_without_id(data_dict, web_crawler, index):
+    filtered_values = [value for value in data_dict.values() if value is not None]
+    if filtered_values:
+        search_query = " ".join(filtered_values)
+        search_query = quote(search_query)
+        google_search_url = f'https://www.google.com/search?q={search_query}'
+        web_crawler.crawl_website_with_depth(str(index), 1, start_url=google_search_url)
+    else:
+        print("Dictionary has no valid values to perform a search.")
+
 
 if __name__ == '__main__':
     file_path = 'resources/QualityTest2.xlsx'
@@ -11,27 +29,14 @@ if __name__ == '__main__':
     data = excel.pre_process()
     wc = WebCrawler()
     filename = 'data.csv'
-    i = 1
-    for dictionary in data:
+
+    for idx, dictionary in enumerate(data, start=1):
         if 'id' in dictionary and dictionary['id'] is not None:
-            id_val = dictionary['id']
-            e_nr_url = f'https://www.e-nummersok.se/sok?Query={id_val}'
-            rsk_nr_url = f'https://www.rskdatabasen.se/sok?Query={id_val}'
-            wc.crawl_website_with_depth(str(i), 0, start_url=e_nr_url)
-            wc.crawl_website_with_depth(str(i), 0, start_url=rsk_nr_url)
+            id_nr = dictionary['id']
+            process_with_id(id_nr, wc, idx)
+            process_without_id(dictionary, wc, idx)
         else:
-            #print("No 'id' found. Performing a Google search...")
-            # Filter out None values from the dictionary's values
-            filtered_values = [value for value in dictionary.values() if value is not None]
-            if filtered_values:
-                # Construct a Google search query using the filtered values
-                search_query = " ".join(filtered_values)
-                search_query = quote(search_query)  # URL encode the search query
-                google_search_url = f'https://www.google.com/search?q={search_query}'
-                print(f'Search Query {i}: {google_search_url}')
-                wc.crawl_website_with_depth(str(i), 1, start_url=google_search_url)
-            else:
-                print("Dictionary has no valid values to perform a search.")
-        i += 1
+            process_without_id(dictionary, wc, idx)
+
     print("------Done------")
     wc.close()
