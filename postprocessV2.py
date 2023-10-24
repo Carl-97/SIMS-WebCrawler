15fb3a36bc7f648df8f3c5d9f96e9d81d2274f9c
Carl-97 <cape1903@student.miun.se>
2023-10-24 14:43:11 +0200

    fixes

--- postprocessV2.py
+++ postprocessV2.py
@@ -7,6 +7,8 @@ from openpyxl.styles import PatternFill
 GREEN_FILL = PatternFill(start_color='00FF00', end_color='00FF00', fill_type='solid')
 YELLOW_FILL = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
 RED_FILL = PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
+BLUE_FILL = PatternFill(start_color='0000FF', end_color='0000FF', fill_type='solid')
+
 
 class CSVHandler:
     @staticmethod
